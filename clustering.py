2f3ab99194626f5cc44d553946bf2cf87b35019f
josemoscoso-duke <jose.moscoso@duke.edu>
2021-06-03 12:21:43 -0400

    Add Spectral Clustering algorithm, and generalize logic to apply to any dataset with a certain structure (column names and segmentation (regions))

--- clustering.py
+++ clustering.py
@@ -1,41 +1,37 @@
-#file management
+# file management
 import os
 from pathlib import Path
 
-#data wrangling
+# data wrangling
 import pandas as pd
 import numpy as np
 
-#Create functions for each plot of the clustering methods
-from matplotlib import pyplot as plt
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import SpectralClustering
+# Create functions for each clustering methods
+import sklearn.utils
+from sklearn.cluster import DBSCAN, SpectralClustering
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import StandardScaler
+
 import warnings
 warnings.filterwarnings('ignore')
 
-#mapbase functions
+# mapbase functions
 from mpl_toolkits.basemap import Basemap
 from pylab import rcParams
+#%matplotlib inline
+rcParams['figure.figsize'] = (14,10)
 
+# plotting package
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-%matplotlib inline
-rcParams['figure.figsize'] = (14,10)
-
-
-from sklearn.cluster import DBSCAN, SpectralClustering
-import sklearn.utils
-from sklearn.preprocessing import StandardScaler
-
 import math
 
 # setup path to file with wind farm coordinates
-FILENAME = 'eGrid_wt_coords.csv'
-INPUT_FILEPATH = Path('processed_data/wt')/FILENAME
-OUTPUT_FILEPATH = Path('processed_data/wt')
+# FILENAME = 'eGrid_wt_coords.csv'
+# INPUT_FILEPATH = Path('processed_data/wt')/FILENAME
+# OUTPUT_FILEPATH = Path('processed_data/wt')
 
 # perform clustering
 REGIONS = ['NE', 'NW', 'EM', 'SW'] # Northeast, Northwest, Eastern Midwest
@@ -44,12 +40,16 @@ CLUSTERING = {
     'DBSCAN': {
     'function': DBSCAN,
     'params': {'eps':0.4, 'min_samples':20}
+    },
+    'SC': {
+    'function':SpectralClustering,
+    'params': {'n_clusters': 4, 'affinity':'nearest_neighbors'}
     }
 }
 
 
-def apply_clustering(input_filepath:Path = INPUT_FILEPATH,
-                     output_filepath:Path = OUTPUT_FILEPATH,
+def apply_clustering(input_filepath:Path,
+                     output_filepath:Path,
                      region: str='NE', method: str='DBSCAN') -> pd.DataFrame:
                      """
                      Takes as input a csv file with column names name, lon, lat,
@@ -65,11 +65,11 @@ def apply_clustering(input_filepath:Path = INPUT_FILEPATH,
                         named cluster with corresponding cluster.
                      """
                      #download csv file
-                     wt_df = pd.read_csv(INPUT_FILEPATH)
+                     df = pd.read_csv(input_filepath)
 
                      if region in ['NE', 'NW', 'EM', 'SW']:
                          print(f'clustering {region} region')
-                         wt_df = wt_df.loc[wt_df.region == region]
+                         df = df.loc[df.region == region]
 
                      elif region is None:
                          print(f'clustering across all regions')
@@ -78,9 +78,9 @@ def apply_clustering(input_filepath:Path = INPUT_FILEPATH,
                          print(f'{region} is an invalid region [NE, NW, EM, SW]')
                          return
 
-                     wt_df_lon_lat = wt_df[["lon", "lat"]]
+                     df_lon_lat = df[["lon", "lat"]]
                      # scaling coordinates before fitting the cluster method
-                     wt_scaled = StandardScaler().fit_transform(wt_df_lon_lat)
+                     df_scaled = StandardScaler().fit_transform(df_lon_lat)
 
                      # fit clustering with eps and min_samples
                      #if CLUSTERING[cluster_method].lower() == 'dbscan':
@@ -89,24 +89,23 @@ def apply_clustering(input_filepath:Path = INPUT_FILEPATH,
                      cluster_function = CLUSTERING[method]['function']
                      params = CLUSTERING[method]['params']
 
-                     cluster = cluster_function(**params).fit(wt_scaled)
+                     cluster = cluster_function(**params).fit(df_scaled)
                      labels = cluster.labels_
-                     wt_df["cluster"]=labels
+                     df["cluster"]=labels
                      num_labels = set(labels)
                      realClusterNum=len(num_labels) - (1 if -1 in labels else 0)
                      clusterNum = len(num_labels)
                      print(f'There are {realClusterNum} clusters, excluding noise')
                      for clust_number in num_labels:
-                         clust_set = wt_df[wt_df.cluster == clust_number]
+                         clust_set = df[df.cluster == clust_number]
                          print(f'cluster {clust_number} has {len(clust_set)} records')
-                     wt_df.to_csv(os.path.join(OUTPUT_FILEPATH,
+                     df.to_csv(os.path.join(output_filepath,
                                   f'{region}_clusters.csv'), index=False)
-                     return wt_df
+                     return df
 
 
-def plot_clusters(region: str='NE',
-                  input_filepath:Path = INPUT_FILEPATH,
-                  output_filepath:Path = OUTPUT_FILEPATH):
+def plot_clusters(input_filepath:Path,
+                  output_filepath:Path, region: str='NE'):
     """
     Takes as input a csv file produced by the apply_clustering function:
     Arguments:
@@ -117,12 +116,12 @@ def plot_clusters(region: str='NE',
     """
 
     FILENAME = f'{region}_clusters.csv'
-    filepath = INPUT_FILEPATH/FILENAME
-    wt_df = pd.read_csv(filepath)
+    filepath = input_filepath/FILENAME
+    df = pd.read_csv(filepath)
     # set area to plot
-    print(wt_df.describe().loc[['min', 'max']])
+    print(df.describe().loc[['min', 'max']])
 
-    stats = wt_df.describe()
+    stats = df.describe()
     llon = math.floor(stats.at['min', 'lon']-1)
     ulon = math.ceil(stats.at['max', 'lon']+1)
     llat = math.floor(stats.at['min', 'lat']-0.5)
@@ -141,19 +140,19 @@ def plot_clusters(region: str='NE',
     map.bluemarble()
 
     # To create a color map
-    labels = set(wt_df.cluster)
+    labels = set(df.cluster)
     clusterNum = len(labels)
     colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, clusterNum))
 
     # collect data based on wind turbines
-    xs,ys = map(np.asarray(wt_df.lon), np.asarray(wt_df.lat))
-    wt_df['xm']= xs.tolist()
-    wt_df['ym'] =ys.tolist()
+    xs,ys = map(np.asarray(df.lon), np.asarray(df.lat))
+    df['xm']= xs.tolist()
+    df['ym'] =ys.tolist()
 
     #Visualization2
     for clust_number in labels:
         c=(([0.4,0.4,0.4]) if clust_number == -1 else colors[np.int(clust_number)])
-        clust_set = wt_df[wt_df.cluster == clust_number]
+        clust_set = df[df.cluster == clust_number]
         print(f'cluster {clust_number} has {len(clust_set)} records')
         map.scatter(clust_set.xm, clust_set.ym, color=c,  marker='o',
                     s= 40, alpha = 0.65)
@@ -165,13 +164,13 @@ def plot_clusters(region: str='NE',
             #print ("Cluster "+str(clust_number))
     plt.title(f'Clusters of collected images for wind turbines in the {region} '
               f'region', fontsize=14)
-    plt.savefig(os.path.join(OUTPUT_FILEPATH,f'{region}_cluster.png'), dpi=300)
+    plt.savefig(os.path.join(output_filepath,f'{region}_cluster.png'), dpi=300)
     return
 
 
-def stratified_split(region: str='NE',
-                     input_filepath:Path = INPUT_FILEPATH,
-                     output_filepath:Path = OUTPUT_FILEPATH,
+def stratified_split(input_filepath:Path,
+                     output_filepath:Path,
+                     region: str='NE',
                      strata_column: str = 'cluster', n_splits: int = 5,
                      train_size: int = 100, test_size: int = 100,
                      random_state: int = 42) -> None:
@@ -191,7 +190,7 @@ def stratified_split(region: str='NE',
                      """
 
                      FILENAME = f'{region}_clusters.csv'
-                     filepath = INPUT_FILEPATH/FILENAME)
+                     filepath = input_filepath/FILENAME
                      data = pd.read_csv(filepath)
 
                      #drop noise in clusters
@@ -216,9 +215,9 @@ def stratified_split(region: str='NE',
                      print('test data cluster distribution:')
                      print(test_set.cluster.value_counts(normalize=True))
 
-                     train_set.to_csv(os.path.join(OUTPUT_FILEPATH,
+                     train_set.to_csv(os.path.join(output_filepath,
                                   f'{region}_train.csv'), index=False)
 
-                     test_set.to_csv(os.path.join(OUTPUT_FILEPATH,
+                     test_set.to_csv(os.path.join(output_filepath,
                                  f'{region}_test.csv'), index=False)
                      return
